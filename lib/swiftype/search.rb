98f9dff5d615ae8ad9de7f777c9bcf7666fc4044
Matt Riley <mriley@gmail.com>
2012-02-16 16:01:32 -0800

    adding functional boost support and refactoring parameter encoding

--- lib/swiftype/search.rb
+++ lib/swiftype/search.rb
@@ -1,7 +1,7 @@
 module Swiftype
   module Search
     
-    VALID_SEARCH_OPTIONS = [:fetch_fields, :search_fields, :filters, :per_page, :page, :document_types]
+    VALID_SEARCH_OPTIONS = [:fetch_fields, :search_fields, :filters, :per_page, :page, :document_types, :functional_boosts]
     VALID_SUGGEST_OPTIONS = [:fetch_fields, :search_fields, :filters, :document_types]
 
     def parse_search_options(options)
@@ -14,23 +14,23 @@ module Swiftype
 
     def parse_options(options,valid_options)
       parsed_options = {}
-      valid_options.each do |option|
-        next unless options[option]
-        if options[option].instance_of?(Array)
-          parsed_options["#{option}[]"] = options[option] 
-        elsif options[option].instance_of?(Hash)
-          options[option].each do |k,v| 
-            if v.instance_of?(Array)
-              parsed_options["#{option}[#{k}][]"] = v 
-            else
-              parsed_options["#{option}[#{k}]"] = v 
-            end
-          end
-        else
-          parsed_options[option] = options[option]
-        end
+      valid_options.each do |option_name|
+        next unless options[option_name]        
+        encode_single_option(parsed_options,option_name,options[option_name])
       end
       parsed_options
+    end
+    
+    # recursive method to encode arrays, hashes, and values the 'Rails' way, to make server-side parsing simpler
+    def encode_single_option(parsed_options,key,value,prefix='')
+      prefix = key if prefix.empty?
+      if value.instance_of?(Hash)
+        value.each { |k,v| encode_single_option(parsed_options,k,v,"#{prefix}[#{k}]") }
+      elsif value.instance_of?(Array)
+        parsed_options["#{prefix}[]"] = value
+      else
+        parsed_options["#{prefix}"] = value
+      end
     end
 
   end
