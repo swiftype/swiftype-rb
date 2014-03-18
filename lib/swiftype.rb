58bdcabf11975a56df8cbd62680728ef12cc4389
Andrew Geweke <andrew@geweke.org>
2014-03-18 12:05:55 -0700

    v0.0.6 of the old 'swiftype-rb' gem: add a deprecation notice telling people not to use that gem any longer.

--- lib/swiftype.rb
+++ lib/swiftype.rb
@@ -14,7 +14,7 @@ module Swiftype
   autoload :DocumentType, 'swiftype/document_type'
   autoload :Document, 'swiftype/document'
   autoload :Easy, 'swiftype/easy'
-  
+
 
   extend Configuration
 
@@ -31,6 +31,16 @@ module Swiftype
     def respond_to?(method, include_private=false)
       new.respond_to?(method, include_private) || super(method, include_private)
     end
+
+    def deprecation_notice!
+      ActiveSupport::Deprecation.warn(
+        %{You're using a very old client to access Swiftype. The gem named 'swiftype-rb' is deprecated,
+has not received updates since August 2012, and will receive no further updates.
+
+Please switch to using the 'swiftype' gem instead, at your convenience; it is far
+better maintaned, supported, and documented.}, caller)
+    end
   end
 end
 
+::Swiftype.deprecation_notice!
